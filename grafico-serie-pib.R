25d5e069d837a932dd673656a2ae46868df2e926
pedro.duarte <pedro.duarte@take.net>
2022-03-29 10:40:46 -0300

    Some important notes, and a stacionary time-series graph

--- grafico-serie-pib.R
+++ grafico-serie-pib.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(readxl)
 library(lubridate)
+library(forecast)
 
 dados <- read_excel("serie-pib-brasil.xlsx")
 dados$PIB <- as.double(dados$PIB)
@@ -8,11 +9,64 @@ dados$Ano <- as.integer(dados$Ano)
 
 fonte <- "Fonte: Banco Mundial Data."
 
+serie <- ts(dados$PIB, start = 1961, frequency = 1)
 
-dados %>% 
-  arrange(Ano) %>% 
-  ggplot() +
-  geom_line(
-    aes(x = Ano, y = PIB)
+plot.ts(serie)
+
+
+
+autoplot(gold) +
+  labs(
+    y = "",
+    x = "",
+    title = "Evolução do preço internacional do Ouro (por Kg)"
+  )
+
+
+set.seed(10)
+pl <- plot.ts(arima.sim(list(1, 1, 0), n = 120), ylab = NULL)
+
+png("serie-arima.png", width = 2200, height = 1300, res = 300, type = "cairo")
+set.seed(10)
+plot.ts(arima.sim(list(1, 1, 0), n = 120), ylab = NULL)
+dev.off()
+
+
+
+
+
+### Um exemplo de série temporal estacionária:
+set.seed(10)
+x <- arima.sim(list(1, 1, 0), n = 120)
+
+
+pl <- ggplot(data = data.frame(x = seq_along(as.double(x)), y = as.double(x))) +
+  geom_hline(
+    size = 1,
+    color = "steelblue",
+    yintercept = mean(as.double(x))
   ) +
-  scale_y_continuous(limits = c(0, 1e15))
+  geom_line(
+    aes(x = x, y = y)
+  ) +
+  labs(title = "Exemplo de série estacionária") +
+  theme_classic() +
+  theme(
+    axis.title = element_blank(),
+    axis.text = element_text(color = "#222222", size = 12),
+    plot.title = element_text(color = "#222222", size = 17, face = "bold", margin = margin(b = 20)),
+    plot.title.position = "plot"
+  )
+
+png("exemplo-serie-estacionaria.png", width = 2200, height = 1300, res = 300, type = "cairo")
+print(pl)
+dev.off()
+
+
+### Podemos utilizar um teste ADF (Augmented Dickey-Fuller) para testarmos
+### se essa série é de fato estacionária.
+
+### O pacote `urca` nos oferece a função `ur.df()` que é capaz de calcular
+### tal teste. Perceba pelos resultados do teste calculado abaixo, que a série `x` é
+### de fato uma série estacionária.
+urca::summary(urca::ur.df(x, type = "none"))
